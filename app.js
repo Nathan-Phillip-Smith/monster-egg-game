5b5c99af9d099317d047a5cecebb3e8e7c472b0f
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-02-18 12:00:51 -0700

    made obstacles unabel to overlap

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ window.addEventListener('load', function() {
             this.game = game;
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
-            this.collisionRadius = 30;
+            this.collisionRadius = 50;
             this.speedX = 0;
             this.speedY = 0;
             this.dx = 0;
@@ -73,7 +73,7 @@ window.addEventListener('load', function() {
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this);
-            this.numberOfObstacles = 5;
+            this.numberOfObstacles = 10;
             this.obstacles = [];
             this.mouse = {
                 x: this.width * 0.5,
@@ -106,8 +106,24 @@ window.addEventListener('load', function() {
             this.obstacles.forEach(obstacle => obstacle.draw(context)); 
         }
         init(){
-            for(let i = 0; i < this.numberOfObstacles; i++){
-                this.obstacles.push(new Obstacle(this));
+            let attempts = 0;
+            while (this.obstacles.length < this.numberOfObstacles && attempts < 500){
+                let testObstacle = new Obstacle(this);
+                let overlap = false;
+                this.obstacles.forEach(obstacle => {
+                    const dx = testObstacle.collisionX - obstacle.collisionX;
+                    const dy = testObstacle.collisionY - obstacle.collisionY;
+                    const distance = Math.hypot(dy, dx);
+                    const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius;
+                    if (distance < sumOfRadii){
+                        overlap = true;
+                    }
+                });
+                if (!overlap){
+                    this.obstacles.push(testObstacle);
+                }
+                attempts++;
+
             }
         }
 
